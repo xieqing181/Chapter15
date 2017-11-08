a5c5be3aed198abf8759a8cfea9ca2d242f97d2d
xieqing181 <33027473+xieqing181@users.noreply.github.com>
2017-11-08 16:41:12 +0800

    Add files via upload

---
+++ die_visual.py
@@ -0,0 +1,30 @@
+from die import Die
+import pygal
+
+die = Die()
+
+results = []
+
+for roll_num in range(1000):
+	result = die.roll()
+	results.append(result)
+	
+#analyse the result
+frequencies = []
+
+for value in range(1, die.num_sides+1):
+	frequency = results.count(value)
+	frequencies.append(frequency)
+	
+#print(frequencies)
+#try to show the result in grafic
+
+hist = pygal.Bar()
+
+hist.title = "Result of rolling D6 1000 times"
+hist.x_labels = ['1', '2', '3', '4', '5', '6']
+hist.x_title = "Result"
+hist.y_title = "Frequency of Result"
+
+hist.add('D6', frequencies)
+hist.render_to_file('die_visual.svg')
